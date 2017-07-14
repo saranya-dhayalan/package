4cdefbd9559fe42dd95d24c2f2ba0cf7651f820a
saranya <saranyadhayalan@syncfusion.com>
2017-07-14 17:49:21 +0530

    initial commit

--- addition.js
+++ addition.js
@@ -1,10 +1,4 @@
-var Add = (function () {
-    function Add() {
-    }
-    Add.prototype.arithmatic = function (a, b) {
-        console.log(a + b);
-    };
-    return Add;
-}());
-var obj = new Add();
-obj.arithmatic(4, 5);
+module exports={
+function add(a,b){
+console.log(a+b);
+}}
